e504920c65db6c3917c73d5d8587959059456cd2
Adekunle Adegbie <kadegbie@gmail.com>
2026-03-14 11:09:54 +0100

    Auto-create student_exam_status table

--- utils/db.py
+++ utils/db.py
@@ -6,4 +6,24 @@ def get_connection():
 
     conn.row_factory = sqlite3.Row
 
+    create_tables(conn)
+
     return conn
+
+
+def create_tables(conn):
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS student_exam_status (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        exam_unlocked BOOLEAN DEFAULT 0,
+        exam_reviewed BOOLEAN DEFAULT 0,
+        last_score INTEGER DEFAULT 0,
+        attempts INTEGER DEFAULT 0
+    )
+    """)
+
+    conn.commit()
